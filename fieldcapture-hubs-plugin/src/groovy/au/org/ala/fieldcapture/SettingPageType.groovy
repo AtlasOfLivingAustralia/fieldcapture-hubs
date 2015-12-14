693843127b8b6a41018e5d0a31633443f2b9fd0d
chrisala <chris.godwin.ala@gmail.com>
2015-12-14 12:25:26 +1100

    Turned the help links title to managed content due to a temporarly required change.

--- fieldcapture-hubs-plugin/src/groovy/au/org/ala/fieldcapture/SettingPageType.groovy
+++ fieldcapture-hubs-plugin/src/groovy/au/org/ala/fieldcapture/SettingPageType.groovy
@@ -49,7 +49,8 @@ enum SettingPageType {
     GREEN_ARMY_REPORT_SUBMITTED_EMAIL_SUBJECT_LINE('greenArmyReportSubmittedSubject', 'Subject line for the \'Report has been submitted\' email', 'fielddata.greenArmyReportSubmitted.emailSubject'),
     GREEN_ARMY_REPORT_APPROVED_EMAIL_SUBJECT_LINE('greenArmyReportApprovedSubject', 'Subject line for the \'Report has been approved\' email', 'fielddata.greenArmyReportApproved.emailSubject'),
     GREEN_ARMY_REPORT_REJECTED_EMAIL_SUBJECT_LINE('greenArmyReportRejectedSubject', 'Subject line for the \'Report has been rejected\' email', 'fielddata.greenArmyReportRejected.emailSubject'),
-    THIRD_PARTY_PHOTO_CONSENT_DECLARATION('thirdPartyPhotoConsent', 'Declaration text for consent from third parties who appear in images', 'fielddata.thirdPartyConsent.text')
+    THIRD_PARTY_PHOTO_CONSENT_DECLARATION('thirdPartyPhotoConsent', 'Declaration text for consent from third parties who appear in images', 'fielddata.thirdPartyConsent.text'),
+    HELP_LINKS_TITLE('helpLinksTitle', 'Title for the Helpful Links section of the homepage', 'fielddata.helpLinksTitle.text')
     String name
     String title
     String key
